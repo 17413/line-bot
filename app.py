dc9a41e2a6456099ccf632dc6864b19a3b308cd3
kinoshita <17413@toba-cmt.ac.jp>
2021-03-12 11:53:15 +0900

    aa

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def handle_message(event):
                 ImageSendMessage(img_url, img_url)
             ]
          )             
-    elif "柴犬" in text:
+    elif "柴" in text:
         img_url = "https://17413.azurewebsites.net/HAckason/%E8%8A%9D.jpg"
         img_url2 = "https://17413.azurewebsites.net/HAckason/%E3%81%97%E3%81%AF%E3%82%99.png"
         line_bot_api.reply_message(
